448986733cbf1a3ab094922a2046715b99ea71ae
Trabalho-Montanha-Otaviano <123115047@ulife.com.br>
2024-10-23 22:39:46 -0300

    Update QBank.sql

--- QBank.sql
+++ QBank.sql
@@ -1,18 +1,18 @@
-CREATE TABLE "mytable" (
-  "CREATE DATABASE QBank;" text
-);
 
-INSERT INTO "mytable" ("CREATE DATABASE QBank;")
-VALUES
-('USE QBank;'),
-('CREATE TABLE Conta ('),
+CREATE DATABASE QBank;
+
+USE QBank;
+
+CREATE TABLE Conta ('),
 ('ID_Conta INT PRIMARY KEY AUTO_INCREMENT'),
 ('Nome_Cliente VARCHAR(100) NOT NULL'),
 ('CPF VARCHAR(11) UNIQUE NOT NULL'),
 ('Saldo DECIMAL(15'),
 ('Data_Criacao DATE NOT NULL'),
-(');'),
-('CREATE TABLE Transacao ('),
+(');
+
+  
+  CREATE TABLE Transacao ('),
 ('ID_Transacao INT PRIMARY KEY AUTO_INCREMENT'),
 ('ID_Conta_Origem INT'),
 ('ID_Conta_Destino INT'),
@@ -21,8 +21,10 @@ VALUES
 ('Tipo_Transacao VARCHAR(50) NOT NULL'),
 ('FOREIGN KEY (ID_Conta_Origem) REFERENCES Conta(ID_Conta)'),
 ('FOREIGN KEY (ID_Conta_Destino) REFERENCES Conta(ID_Conta)'),
-(');'),
-('CREATE TABLE Emprestimo ('),
+(');
+
+  
+  CREATE TABLE Emprestimo ('),
 ('ID_Emprestimo INT PRIMARY KEY AUTO_INCREMENT'),
 ('ID_Conta INT'),
 ('Valor DECIMAL(15'),
@@ -30,16 +32,18 @@ VALUES
 ('Juros DECIMAL(5'),
 ('Prazo INT NOT NULL'),
 ('FOREIGN KEY (ID_Conta) REFERENCES Conta(ID_Conta)'),
-(');'),
-('CREATE TABLE CartaoCredito ('),
+(');
+  
+  CREATE TABLE CartaoCredito ('),
 ('ID_Cartao INT PRIMARY KEY AUTO_INCREMENT'),
 ('ID_Conta INT'),
 ('Limite DECIMAL(15'),
 ('Data_Aprovacao DATE NOT NULL'),
 ('Fatura_Atual DECIMAL(15'),
 ('FOREIGN KEY (ID_Conta) REFERENCES Conta(ID_Conta)'),
-(');'),
-('CREATE TABLE PagamentoServico ('),
+(');
+    
+  CREATE TABLE PagamentoServico ('),
 ('ID_Pagamento INT PRIMARY KEY AUTO_INCREMENT'),
 ('ID_Conta INT'),
 ('Servico VARCHAR(100) NOT NULL'),
